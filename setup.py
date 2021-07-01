c5aea0bc2e7e8b719cffeb653bb01f268dd2c0a4
Raj Srikar <rajsrikar.d161@gmail.com>
2021-07-01 15:41:48 +0530

    Name change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pysudoku",
+    name="solvesudoku",
     version="0.0.1",
     author="Raj Srikar",
     author_email="rajsrikar.d161@gmail.com",
