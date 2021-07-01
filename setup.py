31db99dadabb6b7eefecbe1eacf747fd3e7fd18a
Raj Srikar <rajsrikar.d161@gmail.com>
2021-07-01 16:06:48 +0530

    py build 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="solvesudoku",
-    version="0.0.1",
+    version="0.0.2",
     author="Raj Srikar",
     author_email="rajsrikar.d161@gmail.com",
     description="Solves Sudoku within seconds.",
